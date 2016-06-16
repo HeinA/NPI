1cbfa76157a178b9c6c952a344b4c2695cb9844a
Hein Albrecht <hvalbrecht@gmail.com>
2016-06-16 15:05:16 +0100

    ...

---
+++ Source/Picking.Mobile/Form1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using System.ServiceModel;
+
+namespace Picking.Mobile
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            BasicHttpBinding binding = new BasicHttpBinding();
+            EndpointAddress ea = new EndpointAddress("http://dred-msi/PickingService/PickingService.svc");
+            //PickingService.PickingSlip ps2 = new Picking.Mobile.PickingService.PickingSlip();
+            //PickingService.PickingService ps = new Picking.Mobile.PickingService.PickingService();
+            //var ps1 = ps.GetPickingSlip("sadfsadf");
+        }
+    }
+}
