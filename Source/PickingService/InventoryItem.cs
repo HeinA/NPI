1b6226ffda3d100fc6843c6fe01423440775d6ac
Hein Albrecht <hvalbrecht@gmail.com>
2016-06-16 15:06:16 +0100

    ...

---
+++ Source/PickingService/InventoryItem.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PickingService
+{
+  [DataContract]
+  public class InventoryItem
+  {
+    [DataMember]
+    public string ItemCode { get; set; }
+
+    [DataMember]
+    public decimal Quantity { get; set; }
+  }
+}
---
+++ Source/PickingService/PickingSlip.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PickingService
+{
+  [DataContract]
+  public class PickingSlip
+  {
+    [DataMember]
+    public string DocumentNumber { get; set; }
+
+    Collection<InventoryItem> mItems;
+    [DataMember]
+    public Collection<InventoryItem> Items
+    {
+      get { return mItems ?? (mItems = new Collection<InventoryItem>()); }
+    }
+  }
+}
